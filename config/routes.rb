8a4f7724b13af4505ad4bf8e3c19314baf12502d
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-01-21 04:13:38 +0530

    install devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :notes
 
   get "pages/:name" => "pages#show"
 
-  root "pages#home"
+  root "notes#index"
 
   get "inertia-example", to: "inertia_example#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
